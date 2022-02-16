3f0db7ecc26d083550300960573e9a16d47c09a6
Chase Faggard <chasefaggard@Chases-MacBook-Pro.local>
2022-02-16 15:41:23 -0600

    Added live heroku server url

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -11,7 +11,7 @@ import User from './Classes/User'
 })
 export class SocketService {
 
-  private socket: Socket = io(environment.production ? '' : 'http://localhost:8080')
+  private socket: Socket = io(environment.production ? 'https://chat-io-server.herokuapp.com/' : 'http://localhost:8080')
 
   constructor() { }
 
